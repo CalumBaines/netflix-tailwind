2b8a164b7800be3abede844fb7ad70a6ff6d5b46
Calum Baines <calumrhysbaines@gmail.com>
2021-02-21 15:44:51 +0000

    add list component

---
+++ components/PopularList.vue
@@ -0,0 +1,52 @@
+<template>
+  <section
+    class="absolute bottom-0 w-full bg-gradient-to-t from-black to-transparent"
+  >
+    <div class="px-16">
+      <h2 class="text-2xl font-bold text-white">Popular on Netflix</h2>
+    </div>
+    <div class="w-full px-16 py-4 flex overflow-x-auto">
+      <img
+        class="w-1/6 pr-1 rounded"
+        src="~/assets/images/thumbnail.jpg"
+        alt="Parks rec"
+      />
+      <img
+        class="w-1/6 pr-1 rounded"
+        src="~/assets/images/thumbnail-2.webp"
+        alt="Parks rec"
+      />
+      <img
+        class="w-1/6 pr-1 rounded"
+        src="~/assets/images/thumbnail-3.webp"
+        alt="Parks rec"
+      />
+      <img
+        class="w-1/6 pr-1 rounded"
+        src="~/assets/images/thumbnail-4.jpg"
+        alt="Parks rec"
+      />
+      <img
+        class="w-1/6 pr-1 rounded"
+        src="~/assets/images/thumbnail-5.webp"
+        alt="Parks rec"
+      />
+      <img
+        class="w-1/6 pr-1 rounded"
+        src="~/assets/images/thumbnail-6.webp"
+        alt="Parks rec"
+      />
+      <img
+        class="w-1/6 pr-1 rounded"
+        src="~/assets/images/thumbnail-7.jpg"
+        alt="Parks rec"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,57 +2,15 @@
   <div>
     <Header />
     <Hero />
-    <section
-      class="absolute bottom-0 w-full bg-gradient-to-t from-black to-transparent"
-    >
-      <div class="px-16">
-        <h2 class="text-2xl font-bold text-white">Popular on Netflix</h2>
-      </div>
-      <div class="w-full px-16 py-4 flex overflow-x-auto">
-        <img
-          class="w-1/6 pr-1 rounded"
-          src="~/assets/images/thumbnail.jpg"
-          alt="Parks rec"
-        />
-        <img
-          class="w-1/6 pr-1 rounded"
-          src="~/assets/images/thumbnail-2.webp"
-          alt="Parks rec"
-        />
-        <img
-          class="w-1/6 pr-1 rounded"
-          src="~/assets/images/thumbnail-3.webp"
-          alt="Parks rec"
-        />
-        <img
-          class="w-1/6 pr-1 rounded"
-          src="~/assets/images/thumbnail-4.jpg"
-          alt="Parks rec"
-        />
-        <img
-          class="w-1/6 pr-1 rounded"
-          src="~/assets/images/thumbnail-5.webp"
-          alt="Parks rec"
-        />
-        <img
-          class="w-1/6 pr-1 rounded"
-          src="~/assets/images/thumbnail-6.webp"
-          alt="Parks rec"
-        />
-        <img
-          class="w-1/6 pr-1 rounded"
-          src="~/assets/images/thumbnail-7.jpg"
-          alt="Parks rec"
-        />
-      </div>
-    </section>
+    <popular-list />
   </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue'
 import Hero from '../components/Hero.vue'
+import PopularList from '../components/PopularList.vue'
 export default {
-  components: { Header, Hero },
+  components: { Header, Hero, PopularList },
 }
 </script>
